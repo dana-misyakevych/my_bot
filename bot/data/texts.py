ec619b374c5447330f2a54d88f7c633522330d5a
olehharasymiuk <olegmagot@gmail.com>
2023-02-25 23:24:46 +0200

    extend locale

--- bot/data/texts.py
+++ bot/data/texts.py
@@ -1,12 +1,9 @@
 from bot.middlewares.locale_middleware import get_text as _
 
 answers = [
-    _('"Just a link enough 😤"'),
-
-
+    _("Just a link enough 😤"),
 
 ]
-
 
 reply_to_start_tracking = [
     _("Started following 🫡"),
@@ -14,10 +11,10 @@ reply_to_start_tracking = [
 ]
 
 HELP_COMMAND = _("Greetings! To work with the bot, just drop the link\n"
-"for the product you are interested in\n"
-"\n"
-"The following commands are also available to you:\n"
-"<b>/start</b> - <em>Getting started with the bot</em>\n"
-"<b>/cart</b> - <em>List of products you are following</em>\n"
-"<b>/language</b> - <em>Language</em>\n"
-"<b>/stores</b> - <em>List of available stores</em>")
+                 "for the product you are interested in\n"
+                 "\n"
+                 "The following commands are also available to you:\n"
+                 "<b>/start</b> - <em>Getting started with the bot</em>\n"
+                 "<b>/cart</b> - <em>List of products you are following</em>\n"
+                 "<b>/language</b> - <em>Language</em>\n"
+                 "<b>/stores</b> - <em>List of available stores</em>")
